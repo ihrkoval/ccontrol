c38ab8dd529e50d47583ab65cae6215df0147314
koval ihor <kovalihorsmail@gmail.com>
2017-02-16 12:37:04 +0200

    asd

--- ccontrol-v0.3/src/main/java/com/ccontrol/controllers/TestController.java
+++ ccontrol-v0.3/src/main/java/com/ccontrol/controllers/TestController.java
@@ -1,5 +1,29 @@
 package com.ccontrol.controllers;
 
-public class TestController {
+import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+public class TestController {
+	
+	@RequestMapping(value = "/page", method = RequestMethod.POST)
+    @ResponseBody
+    public String megoggeto(HttpServletRequest request) throws IOException{
+		ParseBody pb = new ParseBody();
+		pb.getPage(request);	
+	    return "ok";
+    }
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String test(HttpServletRequest request) throws IOException{
+					
+	    return "OK!";
+    }
 }
