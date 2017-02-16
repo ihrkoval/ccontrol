50692f6b6d7011a21e6285acd3ca6800dbd6fd0b
koval ihor <kovalihorsmail@gmail.com>
2017-02-16 14:03:17 +0200

    asdfasdf

--- ccontrol-v0.3/src/main/java/com/ccontrol/controllers/ParseBody.java
+++ ccontrol-v0.3/src/main/java/com/ccontrol/controllers/ParseBody.java
@@ -1,17 +1,23 @@
 package com.ccontrol.controllers;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
 public class ParseBody {
+	ServletContext servletContext;
+	
 	public String getPage(HttpServletRequest request) throws IOException{
 		String body = null;
 	    StringBuilder stringBuilder = new StringBuilder();
 	    BufferedReader bufferedReader = null;
+	    
 
 	    try {
 	        InputStream inputStream = request.getInputStream();
@@ -43,9 +49,23 @@ public class ParseBody {
 	}
 	
 	private void parse(String body){
-		int index = body.indexOf(" user = {\"is_authenticated\":true,\"data\"");
+		int index = body.indexOf("user = {\"is_authenticated\":true,\"data\"");
 		int indexEnd = body.indexOf("defaultDomain = 'megogo.net',");
 		String id = body.substring(index, indexEnd);
 		System.out.println(id);
+		try {
+			save(id);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
+	public void save(String s) throws IOException{
+		File f = new File(servletContext.getRealPath("/")+"resources/result/t.txt");
+		FileWriter fw = new FileWriter(f, true);
+		fw.write(s+"/n/n");
+		fw.close();
+	}
+	
 }
